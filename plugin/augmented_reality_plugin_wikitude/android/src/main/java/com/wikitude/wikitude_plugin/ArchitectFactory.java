e6d815169906c6015adc30a5b2465e4342dc4788
francis <Francis365@outlook.com>
2024-08-30 11:31:51 +0100

    Removed registrar

--- plugin/augmented_reality_plugin_wikitude/android/src/main/java/com/wikitude/wikitude_plugin/ArchitectFactory.java
+++ plugin/augmented_reality_plugin_wikitude/android/src/main/java/com/wikitude/wikitude_plugin/ArchitectFactory.java
@@ -1,3 +1,55 @@
+//package com.wikitude.wikitude_plugin;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//
+//import io.flutter.plugin.common.StandardMessageCodec;
+//import io.flutter.plugin.platform.PlatformView;
+//import io.flutter.plugin.platform.PlatformViewFactory;
+//
+//import static io.flutter.plugin.common.PluginRegistry.Registrar;
+//
+//public class ArchitectFactory extends PlatformViewFactory {
+//
+//    private final Registrar mPluginRegistrar;
+//    private final Activity activity;
+//
+//    private ArchitectWidget architectWidget;
+//
+//    public ArchitectFactory(Registrar registrar, Activity activity) {
+//        super(StandardMessageCodec.INSTANCE);
+//        mPluginRegistrar = registrar;
+//        this.activity = activity;
+//    }
+//
+//    @Override
+//    public PlatformView create(Context context, int i, Object o) {
+//        architectWidget = new ArchitectWidget(activity, mPluginRegistrar, i, o);
+//        return architectWidget;
+//    }
+//
+//    void captureScreen() {
+//        if (architectWidget != null) {
+//            architectWidget.captureScreen();
+//        }
+//    }
+//
+//    void captureScreenError(String error) {
+//        if (architectWidget != null) {
+//            architectWidget.captureScreenError(error);
+//        }
+//    }
+//
+//    int getExternalStoragePermissionRequestCode() {
+//        if (architectWidget != null) {
+//            return architectWidget.getExternalStoragePermissionRequestCode();
+//        } else {
+//            return 0;
+//        }
+//    }
+//
+//}
+
 package com.wikitude.wikitude_plugin;
 
 import android.app.Activity;
@@ -7,24 +59,21 @@ import io.flutter.plugin.common.StandardMessageCodec;
 import io.flutter.plugin.platform.PlatformView;
 import io.flutter.plugin.platform.PlatformViewFactory;
 
-import static io.flutter.plugin.common.PluginRegistry.Registrar;
-
 public class ArchitectFactory extends PlatformViewFactory {
 
-    private final Registrar mPluginRegistrar;
     private final Activity activity;
 
     private ArchitectWidget architectWidget;
 
-    public ArchitectFactory(Registrar registrar, Activity activity) {
+    // Constructor updated to remove Registrar and use Activity only
+    public ArchitectFactory(Activity activity) {
         super(StandardMessageCodec.INSTANCE);
-        mPluginRegistrar = registrar;
         this.activity = activity;
     }
 
     @Override
     public PlatformView create(Context context, int i, Object o) {
-        architectWidget = new ArchitectWidget(activity, mPluginRegistrar, i, o);
+        architectWidget = new ArchitectWidget(activity, i, o);
         return architectWidget;
     }
 
@@ -47,5 +96,4 @@ public class ArchitectFactory extends PlatformViewFactory {
             return 0;
         }
     }
-
 }
